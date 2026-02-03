e83350024f9b45c4585cb2db9dd9d319f94b6e0a
santzin <santiago.rosa991412@gmail.com>
2026-02-03 09:12:00 -0300

    init

---
+++ terraform/variables.tf
@@ -0,0 +1,38 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name used for resource naming and tagging"
+  type        = string
+  default     = "projetin"
+}
+
+# VPC
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+# EKS
+variable "eks_cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.31"
+}
+
+variable "eks_node_instance_types" {
+  description = "Instance types for Karpenter Spot nodes"
+  type        = list(string)
+  default     = ["t3.medium", "t3a.medium", "t3.large", "t3a.large"]
+}
+
---
+++ terraform/versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "projetin-tf-state"
+    key    = "infra/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
